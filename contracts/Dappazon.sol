78fe2dcee944182e879dc6eecfbf32fc04ec4cda
MaxSM <maxi.sm117@gmail.com>
2023-01-25 00:03:11 -0300

    :zap: Listing test

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -2,14 +2,55 @@
 pragma solidity ^0.8.9;
 
 contract Dappazon {
-  // string public name;
-  address public owner;
+    // string public name;
+    address public owner;
 
-  constructor() {
-    // name = "Dappazon";
-    owner = msg.sender;
-  }
+    struct Item {
+        uint256 id;
+        string name;
+        string category;
+        string image;
+        uint256 cost;
+        uint256 rating;
+        uint256 stock;
+    }
+
+    // Treat the blockchain as a database
+    mapping(uint256 => Item) public items;
+
+    constructor() {
+        // name = "Dappazon";
+        owner = msg.sender;
+    }
+
+    // List products
+    function list(
+        uint256 _id,
+        string memory _name,
+        string memory _category,
+        string memory _image,
+        uint256 _cost,
+        uint256 _rating,
+        uint256 _stock
+    ) public {
+        // Create Item struct
+        Item memory item = Item(
+            _id,
+            _name,
+            _category,
+            _image,
+            _cost,
+            _rating,
+            _stock
+        );
+
+        // Save Item Struct to blockchain
+        items[_id] = item;
+    }
+
+        // Buy products
+
+        // Withdraw funds
+
 }
-
-
 
--- test/Dappazon.js
+++ test/Dappazon.js
@@ -23,6 +23,29 @@ describe("Dappazon", () => {
     it("Sets the owner", async () => {
       expect(await dappazon.owner()).to.equal(deployer.address)
     })
+  })
+
+    describe("Listing", () => {
+      let transaction
+
+      beforeEach(async () => {
+        transaction = await dappazon.connect(deployer).list(
+          1,
+          "Shoes",
+          "Clothing",
+          "IMAGE",
+          1,
+          4,
+          5
+        )
+
+        await transaction.wait()
+      })
+
+      it("Returns item attributes", async () => {
+        const item = await dappazon.items(1)
+        expect(item.id).to.equal(1)
+      })
   
     // it('has a name', async() => {
     //   const name = await dappazon.name()
