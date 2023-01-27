23ce003de4761b0bf0988846721180ac3adbb415
MaxSM <maxi.sm117@gmail.com>
2023-01-27 17:04:24 -0300

    :bomb: Deployed Dappazon Contract at: 0x5FbDB2315678afecb367f032d93F642f64180aa3

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,6 +4,7 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
+const { ethers } = require("hardhat")
 const hre = require("hardhat")
 const { items } = require("../src/items.json")
 
@@ -12,7 +13,32 @@ const tokens = (n) => {
 }
 
 async function main() {
+  // Setup accounts
+  const [deployer] = await ethers.getSigners()
 
+  // Deploy Dappazon
+  const Dappazon = await hre.ethers.getContractFactory("Dappazon")
+  const dappazon = await Dappazon.deploy()
+  await dappazon.deployed()
+
+  console.log(`Deployed Dappazon Contract at: ${dappazon.address}\n`);
+
+  // List items... (This could be in another script)
+  for (let i = 0; i < items.length; i++) {
+    const transaction = await dappazon.connect(deployer).list(
+      items[i].id,
+      items[i].name,
+      items[i].category,
+      items[i].image,
+      tokens(items[i].price),
+      items[i].rating,
+      items[i].stock,
+    )
+
+    await transaction.wait()
+
+    console.log(`Listed item ${items[i].id}: ${items[i].name}`)
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
