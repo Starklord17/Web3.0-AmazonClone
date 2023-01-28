ac82275a4c9119656fb50df19499c46e0a83f40a
MaxSM <maxi.sm117@gmail.com>
2023-01-27 23:33:46 -0300

    :zap: items

--- src/App.js
+++ src/App.js
@@ -14,7 +14,14 @@ import config from "./config.json";
 
 function App() {
   const [provider, setProvider] = useState(null);
+  
+  const [dappazon, setDappazon] = useState(null);
+  
   const [account, setAccount] = useState(null);
+
+  const [electronics, setElectronics] = useState(null)
+  const [clothing, setClothing] = useState(null)
+  const [toys, setToys] = useState(null)
   
   const loadBlockchainData = async () => {
     // Connect to blockchain
@@ -25,8 +32,26 @@ function App() {
     console.log(network);
 
     // Connect to smart contracts (Create JS Versions)
-    
+    const dappazon = new ethers.Contract( config[network.chainId].dappazon.address, Dappazon, provider );
+    // abi = abstract binary interface
+    setDappazon(dappazon);
+
     // Load products
+
+    const items = []
+
+    for (var i = 0; i < 9; i++) {
+      const item = await dappazon.items(i + 1)
+      items.push(item)
+    }
+    // console.log(items);
+    const electronics = items.filter((item) => item.category === 'electronics')
+    const clothing = items.filter((item) => item.category === 'clothing')
+    const toys = items.filter((item) => item.category === 'toys')
+
+    setElectronics(electronics)
+    setClothing(clothing)
+    setToys(toys)
   };
 
   useEffect(() => {
@@ -38,6 +63,11 @@ function App() {
       <Navigation account={account} setAccount={setAccount}/>
 
       <h2>Dappazon Best Sellers</h2>
+
+      {electronics && clothing && toys && (
+        <p>Products</p>
+      )}
+
 
     </div>
   );
